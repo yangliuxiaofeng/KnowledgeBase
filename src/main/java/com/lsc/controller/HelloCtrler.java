e6ebd7203db5e1174946e91bfa30eec29b22057a
lushaocong <3330809052@qq.com>
2023-03-23 11:30:11 +0800

    add:Swagger接入

--- src/main/java/com/lsc/controller/HelloCtrler.java
+++ src/main/java/com/lsc/controller/HelloCtrler.java
@@ -1,13 +1,18 @@
 package com.lsc.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(value = "HelloController", tags = { "接口" })
 @RequestMapping("/HelloController")
 public class HelloCtrler {
 
-    @RequestMapping("/hello")
+    @ApiOperation(value = "你好")
+    @GetMapping("/hello")
     public String hello(){
         return "hello world";
     }
